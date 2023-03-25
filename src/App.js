048a198fade321e0c3194d3590b8f57f0a87bebc
malikmuhammadshehzad <malikshehzad1534@gmail.com>
2023-03-25 11:32:46 +0500

    first Commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+ import React from 'react'
+import './App.css';
+import Stories from './component/Stories'
+ function App() {
+   return (
+     <>
+     <Stories/>
+     </>
+   )
+ }
+ 
+ export default App
